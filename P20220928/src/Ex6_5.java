9d92de9ada81f5ed99f0e8eb5a945e60b80cec89
mynamejh <112607666+mynamejh@users.noreply.github.com>
2022-10-04 01:16:56 +0900

    commit

---
+++ P20220928/src/Ex6_5.java
@@ -0,0 +1,20 @@
+
+public class Ex6_5 {
+
+	public static void main(String[] args) {
+		guguDan ma = new guguDan();
+		
+		int result = ma.da
+		
+		
+	}
+	
+}
+
+class guguDan{
+	void printGugudan(int dan) {
+		if(!(2<=dan && dan<=9)) {
+			
+		}
+	}
+}
