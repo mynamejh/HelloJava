d8d7670bb1bb060a591d3923861e6cf03439f88a
jihyun <wlgus1598@naver.com>
2022-11-04 17:22:27 +0900

    1104쇼핑몰(장바구니 기능추가)

--- J20221028/src/main/webapp/bootshop/js/shopItem.js
+++ J20221028/src/main/webapp/bootshop/js/shopItem.js
@@ -1,63 +1,69 @@
 //shopItem.js
 
 fetch('../ShopItemListServlet')
-.then(result => result.json())
-.then(itemListShow)
-.catch(err => console.log(err));
+.then( result => result.json())
+.then( itemListShow)
+.catch( err => console.error(err));
 
-
-//>>>>실행되는 부분(main)
 function itemListShow(result){
-	for(let i=0; i<result.length; i++){
-    makeItemDiv(result[i]);
-	}
+    result.forEach(item => {
+        makeItemDIv(item);
+    });
+      //addcart누르면..올라간다함..
+   let addCart = document.querySelectorAll('div.card-footer a');
+   console.log(addCart);
+   //console.log(numClick);
+
+   addCart.forEach(item=>{
+    console.log(item);
+    item.addEventListener('click', addClick);
+   });
+
+   
+    
 }
 
-
-//>>>>>속성만들기
-function makeItemDiv(item={}){
+function makeItemDIv(item = {}) {
 
     let template = document.querySelector('#template>div');
+    
     let good = template.cloneNode(true);
-    good.querySelector('h5').textContent=item.itemName;
-    good.querySelector('img.card-img-top').src='../images/'+item.image;
-    good.querySelector('span.text-muted').textContent=item.originPrice;
-    good.querySelector('span.text-muted').nextSibling.textContent=" "+4000;
-    
-    //리뷰>>
-    let review = item.likeIt;// 4.5점
-    let a = Math.floor(review); //온쪽
-    let b = review-a; //0,.5 반쪽
-    
-    console.log(a,b);
+    good.querySelector('h5').textContent = item.itemName;
+    good.querySelector('img.card-img-top').src = '../images/' + item.image;
+    good.querySelector('span.text-muted').textContent = item.originPrice;
+    good.querySelector('span.text-muted').nextSibling.textContent= " $"+ item.salePrice;
+    //span태그의 그다음 요소
+
+    //review-별점
+    let review = item.likeIt; //4.5
+    let a = Math.floor(review); //온쪽.
+    let b = review - a; //o , 0.5 반쪽별
     for(let i=0; i<a; i++){
-        let div= document.createElement('div');
-        div.className ='bi-star-fill';
-        good.querySelector('div.d-flex').append(div);
-    }
-    if(b){
         let div = document.createElement('div');
-        div.className ='bi-star-half';
+        div.className = 'bi-star-fill'; //<div class='bi-star-fill'>
+        //div.setAttribute('class','bi=star-fill')
         good.querySelector('div.d-flex').append(div);
+    } 
+    //b가 존재하면.. 
+    if(b) {
+        let div = document.createElement('div');
+        div.className = 'bi-star-half';
+        good.querySelector('div.d-flex').append(div); 
     }
-    //목록>>
+    //상품목록.
     let parent = document.querySelector('section.py-5>div>div');
     parent.append(good);
     console.log(good);
-}
 
+    //addCart를 누르면 1씩 증가하는 함수
+    function addClick(e){
+        e.preventDefault();
+        let cart = document.querySelector('nav.navbar>div>div>form.d-flex>button');
+        let numClick = cart.querySelector('span.badge').textContent;
+        numClick ++;
+        
+        cart.querySelector('span.badge').textContent = numClick;
+       }
 
-
-document.querySelector('button[type="submit"]').addEventListener('click', addFunc);
-
-
-function addFunc(e){
-	e.preventDefault();
-    console.log('이거 어디서부터 잘못된거지')
-	let div = document.createElement('a.btn');
-	div.className = 'btn btn-outline-dark mt-auto';
-	let shop='badge bg-dark text-white ms-1 rounded-pill';
-	shop+=1;
-	
 
 }
